d4c6bf35fca23b67433ca10f903dc717326ab904
Bedoor Almutairi <badoor975@outlook.sa>
2022-02-03 15:52:49 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+import { runtime } from 'webpack';
+
+const Nav=()=>{
+  return(
+    <nav>
+      <ul>
+     <li> <Link to="/">Home</Link></li>
+     <li> <Link to="/About-us">Aboutus</Link></li>
+     <li> <Link to="/Contact-us">Contactus</Link></li>
+      </ul>
+    </nav>
+  )
+};
+const Home=()=>{
+  return(
+    <h1>Home</h1>
+  )
+};
+
+const Aboutus=()=>{
+  return(
+    <h1>About-us</h1>
+  )
+};
+
+const Contactus=()=>{
+  return(
+    <h1>Contact-us</h1>
+  )
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Nav />
+     <Routes>
+       <Route  exact path="/" element={<Home />}   />
+       <Route  path="/About-us" element={<Aboutus />}   />
+       <Route  path="/Contact-us" element={<Contactus />}   />
+     </Routes>
     </div>
   );
 }
